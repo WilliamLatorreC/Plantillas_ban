9623c0494ea9b82ad772cc2b7abbd2984cc44b09
William Latorre <75050025+WilliamLatorreC@users.noreply.github.com>
2026-01-05 10:48:48 -0500

    Actualizar plantilla.service.ts

--- frontend/src/app/services/plantilla.service.ts
+++ frontend/src/app/services/plantilla.service.ts
@@ -14,8 +14,8 @@ export interface Plantilla {
   providedIn: 'root'
 })
 export class PlantillaService {
-  private apiUrl = 'http://localhost:3000/api/plantillas';
-  //private apiUrl = 'https://plantillas-ban.onrender.com/api/plantillas';
+  //private apiUrl = 'http://localhost:3000/api/plantillas';
+  private apiUrl = 'https://plantillas-ban.onrender.com/api/plantillas';
   constructor(private http: HttpClient) {}
 
   buscar(q: string) {
